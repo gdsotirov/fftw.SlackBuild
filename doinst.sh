8c9c23fbd8ac8e3b8a78eb02dc8a659761c188fe
Georgi D. Sotirov <gdsotirov@dir.bg>
2011-09-11 10:54:09 +0000

    Add post installation script for installation of the info pages

---
+++ doinst.sh
@@ -0,0 +1,26 @@
+# Update the /usr/info/dir info-database, so that we will see the new
+# "fftw" item in info root structure, if we type "info".
+ErrCount=0
+
+function echo_exit {
+  ((ErrCount += $1))
+  if [ $1 -eq 0 ]; then
+    echo -n "$2 "
+  else
+    echo -n $3
+  fi
+}
+
+if [ -x /usr/bin/install-info ] ; then
+  echo -n "Installing info pages... "
+  install-info --info-dir=/usr/info /usr/info/fftw3.info.gz   2>/dev/null
+  echo_exit $? 0 
+  install-info --info-dir=/usr/info /usr/info/fftw3.info-1.gz 2>/dev/null
+  echo_exit $? 1
+  install-info --info-dir=/usr/info /usr/info/fftw3.info-2.gz 2>/dev/null
+  echo_exit $? 2
+  echo_exit $ErrCount "DONE" "FAILURE"
+  echo
+else
+  echo "WARNING: Info pages cannot be installed!"
+fi
